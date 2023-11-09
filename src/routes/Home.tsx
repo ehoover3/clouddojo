3ff0b18b44ae0a159a2e8668c9d764cff89d0885
Eric Hoover <ehoover3@yahoo.com>
2023-11-09 14:32:56 -0500

    updated home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,55 @@
-export default function Home() {
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const cardStyle: React.CSSProperties = {
+    border: "1px solid #ccc",
+    borderRadius: "8px",
+    padding: "20px",
+    width: "300px",
+    textAlign: "center",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+    marginBottom: "20px",
+  };
+
+  const linkStyle = {
+    display: "inline-block",
+    padding: "8px 16px",
+    borderRadius: "4px",
+    background: "#007BFF",
+    color: "#fff",
+    textDecoration: "none",
+    marginTop: "10px",
+  };
+
   return (
     <div>
-      <h2>Home</h2>
+      <div className='card-container' style={{ display: "flex", justifyContent: "space-around", alignItems: "center", marginTop: "50px" }}>
+        <div className='card' style={cardStyle}>
+          <h3>AWS Certification</h3>
+          <p>Explore AWS certifications and advance your cloud skills.</p>
+          <Link to='/aws' style={linkStyle}>
+            Learn More
+          </Link>
+        </div>
+
+        <div className='card' style={cardStyle}>
+          <h3>Azure Certification</h3>
+          <p>Discover Azure certifications for a successful career in the cloud.</p>
+          <Link to='/azure' style={linkStyle}>
+            Learn More
+          </Link>
+        </div>
+
+        <div className='card' style={cardStyle}>
+          <h3>CompTIA Certification</h3>
+          <p>Get certified with CompTIA and excel in IT and cybersecurity.</p>
+          <Link to='/comptia' style={linkStyle}>
+            Learn More
+          </Link>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
