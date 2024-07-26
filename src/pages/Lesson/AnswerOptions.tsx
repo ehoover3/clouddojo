4617153e27c619e179b8d697aa635abdb68ca8f5
Eric Hoover <ehoover3@yahoo.com>
2024-07-26 01:08:17 -0400

    refactor variable names for cleaner code

--- src/pages/Lesson/AnswerOptions.tsx
+++ src/pages/Lesson/AnswerOptions.tsx
@@ -2,31 +2,31 @@ import React from "react";
 import "./AnswerOptions.css";
 
 interface Option {
-  option: string;
+  answerOption: string;
   explanation: string;
 }
 
 interface AnswerOptionsProps {
-  options: Option[];
+  answerOptions: Option[];
   selectedAnswer: string | null;
   assignedAnswer: string | null;
   onClick: (answer: string) => void;
   disabled: boolean;
 }
 
-const AnswerOptions: React.FC<AnswerOptionsProps> = ({ options, selectedAnswer, assignedAnswer, onClick, disabled }) => {
+const AnswerOptions: React.FC<AnswerOptionsProps> = ({ answerOptions, selectedAnswer, assignedAnswer, onClick, disabled }) => {
   return (
     <div className='answers'>
-      {options.map((option, index) => (
+      {answerOptions.map((answerOption, index) => (
         <div
           key={index}
           className='answer'
           style={{
-            backgroundColor: selectedAnswer === option.option ? "lightblue" : assignedAnswer === option.option ? "grey" : "white",
+            backgroundColor: selectedAnswer === answerOption.answerOption ? "lightblue" : assignedAnswer === answerOption.answerOption ? "grey" : "white",
             cursor: disabled ? "not-allowed" : "pointer",
           }}
-          onClick={() => !disabled && onClick(option.option)}>
-          {option.option}
+          onClick={() => !disabled && onClick(answerOption.answerOption)}>
+          {answerOption.answerOption}
         </div>
       ))}
     </div>
--- src/pages/Lesson/index.tsx
+++ src/pages/Lesson/index.tsx
@@ -1,135 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { useLocation, Link } from "react-router-dom";
-// import AwsCloudPractitionerFoundational from "../../data/questions/aws-cloud-practitioner-foundational.json";
-// import AwsDeveloperAssociate from "../../data/questions/aws-developer-associate.json";
-// import AnswerOptions from "./AnswerOptions";
-// import ProgressBar from "./ProgressBar";
-// import SubmitButton from "./SubmitButton";
-// import Explanation from "./Explanation";
-// import QuestionText from "./QuestionText";
-// import "./Quiz.css";
-
-// export interface Option {
-//   option: string;
-//   explanation: string;
-// }
-
-// export interface Question {
-//   id: number;
-//   text: string;
-//   type: string;
-//   answer: string;
-//   assignedAnswer: string | null;
-//   options: Option[];
-// }
-
-// const getCertificationData = (certParameter: string | null, certTitle: string | null, certLevel: string | null) => {
-//   if (certParameter === "aws") {
-//     if (certTitle === "Cloud Practitioner" && certLevel === "Foundational") {
-//       return AwsCloudPractitionerFoundational;
-//     }
-//     if (certTitle === "Developer" && certLevel === "Associate") {
-//       return AwsDeveloperAssociate;
-//     }
-//   }
-//   return null;
-// };
-
-// const Lesson = () => {
-//   const location = useLocation();
-//   const queryParams = new URLSearchParams(location.search);
-//   const certParameter = queryParams.get("cert");
-//   const certTitle = queryParams.get("title");
-//   const certLevel = queryParams.get("level");
-
-//   const [certification, setCertification] = useState<Question[] | null>(null);
-//   const [questionQueue, setQuestionQueue] = useState<number[]>([]);
-//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-//   const [explanation, setExplanation] = useState<string | null>(null);
-//   const [correctAnswers, setCorrectAnswers] = useState(0);
-//   const [showNext, setShowNext] = useState(false);
-//   const [isAnswerSelected, setIsAnswerSelected] = useState(false);
-//   const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
-//   const [incorrectQuestions, setIncorrectQuestions] = useState<Set<number>>(new Set());
-
-//   useEffect(() => {
-//     const data = getCertificationData(certParameter, certTitle, certLevel);
-//     if (data) {
-//       setCertification(data);
-//       setQuestionQueue(data.map((_, index) => index));
-//     }
-//   }, [certParameter, certTitle, certLevel]);
-
-//   const handleAnswer = (answer: string) => {
-//     setSelectedAnswer(answer);
-//     setIsAnswerSelected(true);
-//   };
-
-//   const handleSubmit = () => {
-//     if (certification && selectedAnswer) {
-//       const currentQuestion = certification[questionQueue[currentQuestionIndex]];
-//       const isCorrect = currentQuestion.answer === selectedAnswer;
-//       const selectedOption = currentQuestion.options.find((option) => option.option === selectedAnswer);
-//       const reason = selectedOption ? selectedOption.explanation : "No explanation available.";
-
-//       setExplanation(reason);
-//       setCorrectAnswers((prev) => (isCorrect ? prev + 1 : prev));
-//       setIncorrectQuestions((prev) => (isCorrect ? new Set([...prev].filter((index) => index !== questionQueue[currentQuestionIndex])) : new Set(prev.add(questionQueue[currentQuestionIndex]))));
-//       setShowNext(true);
-//     }
-//   };
-
-//   const handleNext = () => {
-//     if (certification) {
-//       if (currentQuestionIndex < questionQueue.length - 1) {
-//         setCurrentQuestionIndex((prev) => prev + 1);
-//         clearSelectedAnswer();
-//       }
-
-//       if (incorrectQuestions.size > 0) {
-//         const newQueue = [...questionQueue, ...Array.from(incorrectQuestions)];
-//         setQuestionQueue(newQueue);
-//         setIncorrectQuestions(new Set());
-//       }
-//     }
-//   };
-
-//   const clearSelectedAnswer = () => {
-//     setExplanation(null);
-//     setShowNext(false);
-//     setIsAnswerSelected(false);
-//     setSelectedAnswer(null);
-//   };
-
-//   const currentQuestion = certification ? certification[questionQueue[currentQuestionIndex]] : null;
-
-//   return (
-//     <div className='quiz'>
-//       <ProgressBar current={correctAnswers} total={certification ? certification.length : 0} />
-//       {currentQuestion && (
-//         <div className='question-container'>
-//           <QuestionText text={currentQuestion.text} />
-//           <AnswerOptions options={currentQuestion.options} selectedAnswer={selectedAnswer} assignedAnswer={currentQuestion.assignedAnswer} onClick={handleAnswer} disabled={showNext} />
-//           <Explanation explanation={explanation} />
-//           {showNext ? (
-//             currentQuestionIndex < questionQueue.length - 1 ? (
-//               <SubmitButton showNext={showNext} onClick={handleNext} disabled={!isAnswerSelected} />
-//             ) : (
-//               <Link to={`/learn?cert=${certParameter}`} className='submit-button'>
-//                 Back to Learn
-//               </Link>
-//             )
-//           ) : (
-//             <SubmitButton showNext={showNext} onClick={handleSubmit} disabled={!isAnswerSelected} />
-//           )}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Lesson;
-
 import { useState, useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
 import AwsCloudPractitionerFoundational from "../../data/questions/aws-cloud-practitioner-foundational.json";
@@ -142,7 +10,7 @@ import QuestionText from "./QuestionText";
 import "./Quiz.css";
 
 export interface Option {
-  option: string;
+  answerOption: string;
   explanation: string;
 }
 
@@ -152,7 +20,7 @@ export interface Question {
   type: string;
   answer: string;
   assignedAnswer: string | null;
-  options: Option[];
+  answerOptions: Option[];
 }
 
 const getCertificationData = (certParameter: string | null, certTitle: string | null, certLevel: string | null) => {
@@ -198,7 +66,7 @@ const Lesson = () => {
       setCertification(
         data.map((question) => ({
           ...question,
-          options: shuffleArray(question.options),
+          options: shuffleArray(question.answerOptions),
         }))
       );
       setQuestionQueue(data.map((_, index) => index));
@@ -214,7 +82,7 @@ const Lesson = () => {
     if (certification && selectedAnswer) {
       const currentQuestion = certification[questionQueue[currentQuestionIndex]];
       const isCorrect = currentQuestion.answer === selectedAnswer;
-      const selectedOption = currentQuestion.options.find((option) => option.option === selectedAnswer);
+      const selectedOption = currentQuestion.answerOptions.find((option) => option.answerOption === selectedAnswer);
       const reason = selectedOption ? selectedOption.explanation : "No explanation available.";
 
       setExplanation(reason);
@@ -246,7 +114,7 @@ const Lesson = () => {
     setSelectedAnswer(null);
     if (certification) {
       const currentQuestion = certification[questionQueue[currentQuestionIndex]];
-      currentQuestion.options = shuffleArray(currentQuestion.options);
+      currentQuestion.answerOptions = shuffleArray(currentQuestion.answerOptions);
       setCertification([...certification]);
     }
   };
@@ -259,7 +127,7 @@ const Lesson = () => {
       {currentQuestion && (
         <div className='question-container'>
           <QuestionText text={currentQuestion.text} />
-          <AnswerOptions options={currentQuestion.options} selectedAnswer={selectedAnswer} assignedAnswer={currentQuestion.assignedAnswer} onClick={handleAnswer} disabled={showNext} />
+          <AnswerOptions answerOptions={currentQuestion.answerOptions} selectedAnswer={selectedAnswer} assignedAnswer={currentQuestion.assignedAnswer} onClick={handleAnswer} disabled={showNext} />
           <Explanation explanation={explanation} />
           {showNext ? (
             currentQuestionIndex < questionQueue.length - 1 ? (
