a77b147f85d2734b187e8de8c25d3d271c811c18
Eric Hoover <ehoover3@yahoo.com>
2024-08-25 22:52:40 -0400

    fix order phrase component explanation rendering bug

--- src/components/OrderPhrase.tsx
+++ src/components/OrderPhrase.tsx
@@ -1,4 +1,4 @@
-// import React, { useState } from "react";
+// import React, { useState, useEffect } from "react";
 // import TextDisplay from "./TextDisplay";
 // import Button from "./Button";
 // import Explanation from "./Explanation";
@@ -14,61 +14,24 @@
 // }
 
 // const OrderPhrase: React.FC<OrderPhraseProps> = ({ quiz, questionsToAsk, currentQuestionIndex, setQuestionsToAsk, setCurrentQuestion, completeQuiz, setCorrectCount }) => {
-//   const [selectedAnswerOptions, setSelectedAnswerOptions] = useState<string[]>([]);
-//   const [submitted, setSubmitted] = useState<boolean>(false);
-//   const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
-
-//   const handleSelectPhrase = (answerText: string) => {
-//     if (selectedAnswerOptions.includes(answerText)) {
-//       setSelectedAnswerOptions(selectedAnswerOptions.filter((item) => item !== answerText));
-//     } else {
-//       setSelectedAnswerOptions([...selectedAnswerOptions, answerText]);
-//     }
-//   };
-
-//   const handleSubmit = () => {
-//     setSubmitted(true);
-//     const isAnswer = JSON.stringify(selectedAnswerOptions) === JSON.stringify(question.answer);
-//     setIsCorrect(isAnswer);
-//     if (isAnswer) {
-//       setCorrectCount((count) => count + 1);
-//     } else {
-//       setQuestionsToAsk((prev) => [...prev, questionsToAsk[currentQuestionIndex]]);
-//     }
-
-//     // Determine next step
-//     const isLastQuestion = currentQuestionIndex >= questionsToAsk.length - 1;
-//     if (isLastQuestion) {
-//       completeQuiz();
-//     } else {
-//       setCurrentQuestion((prev) => prev + 1);
-//     }
-//   };
-
-//   // start
 //   const question = quiz[questionsToAsk[currentQuestionIndex]];
-//   const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
-//   const [explanation, setExplanation] = useState<{ text: string; img: string }>({ text: "", img: "" });
+//   const [selectedAnswerOptions, setSelectedAnswerOptions] = useState<string[]>([]);
+//   const [explanation, setExplanation] = useState<{ text: string; img: string }>({ text: question.explanationText, img: "" });
 //   const [isCheckBtnClicked, setIsCheckButtonClicked] = useState(false);
 //   const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean | null>(null);
 
-//   const handleAnswer = (answer: string) => {
-//     setSelectedAnswer(answer);
+//   const handleSelectPhrase = (answerText: string) => {
+//     if (selectedAnswerOptions.includes(answerText)) setSelectedAnswerOptions(selectedAnswerOptions.filter((item) => item !== answerText));
+//     else setSelectedAnswerOptions([...selectedAnswerOptions, answerText]);
 //   };
 
 //   const handleCheck = () => {
-//     if (selectedAnswer) {
-//       const isCorrect = question.answer?.[0] === selectedAnswer;
-//       const selectedOption = question.answerOptions.find((option: any) => option.answerText === selectedAnswer);
-//       setExplanation({
-//         text: selectedOption ? selectedOption.explanationText : "No explanation available.",
-//         img: selectedOption ? selectedOption.explanationImg : "No img available.",
-//       });
+//     if (selectedAnswerOptions.length !== 0) {
+//       const isCorrect = JSON.stringify(selectedAnswerOptions) === JSON.stringify(question.answer);
+//       const selectedOption = question.answerOptions.find((option: any) => option.answerText === selectedAnswerOptions.join(", "));
 //       setIsCorrectAnswer(isCorrect);
 //       setCorrectCount((prev: any) => (isCorrect ? prev + 1 : prev));
-//       if (!isCorrect) {
-//         setQuestionsToAsk((prev) => [...prev, questionsToAsk[currentQuestionIndex]]);
-//       }
+//       if (!isCorrect) setQuestionsToAsk((prev) => [...prev, questionsToAsk[currentQuestionIndex]]);
 //       setIsCheckButtonClicked(true);
 //     }
 //   };
@@ -82,12 +45,28 @@
 //   };
 
 //   const clearSelectedAnswer = () => {
-//     setExplanation({ text: "", img: "" });
+//     setSelectedAnswerOptions([]);
 //     setIsCheckButtonClicked(false);
-//     setSelectedAnswer(null);
 //     setIsCorrectAnswer(null);
 //   };
-//   // finish
+
+//   useEffect(() => {
+//     const handleKeyDown = async (event: KeyboardEvent) => {
+//       const key = event.key;
+//       if (!isCheckBtnClicked) {
+//         if (key >= "1" && key <= question.answerOptions.length.toString()) {
+//           const index = parseInt(key, 10) - 1;
+//           if (index < question.answerOptions.length) await handleSelectPhrase(question.answerOptions[index].answerText);
+//         }
+//       }
+//       if (key === "Enter" && selectedAnswerOptions.length === question.answer.length && !isCheckBtnClicked) handleCheck();
+//       if (key === "Enter" && isCheckBtnClicked) handleContinue();
+//     };
+//     window.addEventListener("keydown", handleKeyDown);
+//     return () => {
+//       window.removeEventListener("keydown", handleKeyDown);
+//     };
+//   }, [selectedAnswerOptions, isCheckBtnClicked]);
 
 //   return (
 //     <div>
@@ -109,7 +88,7 @@
 //         </div>
 //       </div>
 //       <Explanation explanation={explanation} isCorrectAnswer={isCorrectAnswer} />
-//       <Button text={isCheckBtnClicked ? "Continue" : "Check"} onClick={isCheckBtnClicked ? handleContinue : handleCheck} disabled={!selectedAnswer && !isCheckBtnClicked} className={isCheckBtnClicked ? "btn-green" : selectedAnswer ? "btn-blue" : "btn-gray"} />
+//       <Button text={isCheckBtnClicked ? "Continue" : "Check"} onClick={isCheckBtnClicked ? handleContinue : handleCheck} disabled={selectedAnswerOptions.length == 0 && !isCheckBtnClicked} className={isCheckBtnClicked ? "btn-green" : selectedAnswerOptions.length != 0 ? "btn-blue" : "btn-gray"} />
 //     </div>
 //   );
 // };
@@ -134,31 +113,22 @@ interface OrderPhraseProps {
 const OrderPhrase: React.FC<OrderPhraseProps> = ({ quiz, questionsToAsk, currentQuestionIndex, setQuestionsToAsk, setCurrentQuestion, completeQuiz, setCorrectCount }) => {
   const question = quiz[questionsToAsk[currentQuestionIndex]];
   const [selectedAnswerOptions, setSelectedAnswerOptions] = useState<string[]>([]);
-  const [explanation, setExplanation] = useState<{ text: string; img: string }>({ text: "", img: "" });
+  const [explanation, setExplanation] = useState<{ text: string; img: string }>({ text: question.explanationText, img: "" });
   const [isCheckBtnClicked, setIsCheckButtonClicked] = useState(false);
   const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean | null>(null);
 
   const handleSelectPhrase = (answerText: string) => {
-    if (selectedAnswerOptions.includes(answerText)) {
-      setSelectedAnswerOptions(selectedAnswerOptions.filter((item) => item !== answerText));
-    } else {
-      setSelectedAnswerOptions([...selectedAnswerOptions, answerText]);
-    }
+    if (selectedAnswerOptions.includes(answerText)) setSelectedAnswerOptions(selectedAnswerOptions.filter((item) => item !== answerText));
+    else setSelectedAnswerOptions([...selectedAnswerOptions, answerText]);
   };
 
   const handleCheck = () => {
-    if (selectedAnswerOptions.length === question.answer.length) {
+    if (selectedAnswerOptions.length !== 0) {
       const isCorrect = JSON.stringify(selectedAnswerOptions) === JSON.stringify(question.answer);
       const selectedOption = question.answerOptions.find((option: any) => option.answerText === selectedAnswerOptions.join(", "));
-      setExplanation({
-        text: selectedOption ? selectedOption.explanationText : "No explanation available.",
-        img: selectedOption ? selectedOption.explanationImg : "No img available.",
-      });
       setIsCorrectAnswer(isCorrect);
       setCorrectCount((prev: any) => (isCorrect ? prev + 1 : prev));
-      if (!isCorrect) {
-        setQuestionsToAsk((prev) => [...prev, questionsToAsk[currentQuestionIndex]]);
-      }
+      if (!isCorrect) setQuestionsToAsk((prev) => [...prev, questionsToAsk[currentQuestionIndex]]);
       setIsCheckButtonClicked(true);
     }
   };
@@ -168,14 +138,11 @@ const OrderPhrase: React.FC<OrderPhraseProps> = ({ quiz, questionsToAsk, current
     if (!isLastQuestion) {
       setCurrentQuestion((prev) => prev + 1);
       clearSelectedAnswer();
-    } else {
-      completeQuiz();
-    }
+    } else completeQuiz();
   };
 
   const clearSelectedAnswer = () => {
     setSelectedAnswerOptions([]);
-    setExplanation({ text: "", img: "" });
     setIsCheckButtonClicked(false);
     setIsCorrectAnswer(null);
   };
@@ -186,17 +153,11 @@ const OrderPhrase: React.FC<OrderPhraseProps> = ({ quiz, questionsToAsk, current
       if (!isCheckBtnClicked) {
         if (key >= "1" && key <= question.answerOptions.length.toString()) {
           const index = parseInt(key, 10) - 1;
-          if (index < question.answerOptions.length) {
-            await handleSelectPhrase(question.answerOptions[index].answerText);
-          }
+          if (index < question.answerOptions.length) await handleSelectPhrase(question.answerOptions[index].answerText);
         }
       }
-      if (key === "Enter" && selectedAnswerOptions.length === question.answer.length && !isCheckBtnClicked) {
-        handleCheck();
-      }
-      if (key === "Enter" && isCheckBtnClicked) {
-        handleContinue();
-      }
+      if (key === "Enter" && selectedAnswerOptions.length === question.answer.length && !isCheckBtnClicked) handleCheck();
+      if (key === "Enter" && isCheckBtnClicked) handleContinue();
     };
     window.addEventListener("keydown", handleKeyDown);
     return () => {
@@ -223,8 +184,8 @@ const OrderPhrase: React.FC<OrderPhraseProps> = ({ quiz, questionsToAsk, current
           ))}
         </div>
       </div>
-      <Explanation explanation={explanation} isCorrectAnswer={isCorrectAnswer} />
-      <Button text={isCheckBtnClicked ? "Continue" : "Check"} onClick={isCheckBtnClicked ? handleContinue : handleCheck} disabled={selectedAnswerOptions.length !== question.answer.length && !isCheckBtnClicked} className={isCheckBtnClicked ? "btn-green" : selectedAnswerOptions.length === question.answer.length ? "btn-blue" : "btn-gray"} />
+      {isCheckBtnClicked && <Explanation explanation={explanation} isCorrectAnswer={isCorrectAnswer} />}
+      <Button text={isCheckBtnClicked ? "Continue" : "Check"} onClick={isCheckBtnClicked ? handleContinue : handleCheck} disabled={selectedAnswerOptions.length === 0 && !isCheckBtnClicked} className={isCheckBtnClicked ? "btn-green" : selectedAnswerOptions.length !== 0 ? "btn-blue" : "btn-gray"} />
     </div>
   );
 };
