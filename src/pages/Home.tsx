d6c9ef292663810a277c35a8a0b4beb1fd6da83a
Eric Hoover <ehoover3@yahoo.com>
2024-09-04 00:02:14 -0400

    update home page into a learning path ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,32 @@
+// import { useNavigate } from "react-router-dom";
+
+// function Home() {
+//   const navigate = useNavigate();
+
+//   const handleQuizSelection = (quizType: string) => {
+//     navigate("/lesson", { state: { quizType } });
+//   };
+
+//   return (
+//     <div>
+//       <h1>Learning Path</h1>
+//       <ul>
+//         <li onClick={() => handleQuizSelection("AwsCloudPractitionerFoundational")}>
+//           <span>AwsCloudPractitionerFoundational</span>
+//         </li>
+//         <li onClick={() => handleQuizSelection("AwsDeveloperAssociate")}>
+//           <span>AwsDeveloperAssociate</span>
+//         </li>
+//         <li onClick={() => handleQuizSelection("AzureAIFundamentalsBeginner")}>
+//           <span>AzureAIFundamentalsBeginner</span>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
 import { useNavigate } from "react-router-dom";
 
 function Home() {
@@ -8,19 +37,19 @@ function Home() {
   };
 
   return (
-    <div>
-      <h1>Certifications</h1>
-      <ul>
-        <li onClick={() => handleQuizSelection("AwsCloudPractitionerFoundational")}>
-          <span>AwsCloudPractitionerFoundational</span>
-        </li>
-        <li onClick={() => handleQuizSelection("AwsDeveloperAssociate")}>
-          <span>AwsDeveloperAssociate</span>
-        </li>
-        <li onClick={() => handleQuizSelection("AzureAIFundamentalsBeginner")}>
-          <span>AzureAIFundamentalsBeginner</span>
-        </li>
-      </ul>
+    <div className='container'>
+      <h1>Learning Path</h1>
+      <div className='step-container'>
+        <div className='step' onClick={() => handleQuizSelection("AwsCloudPractitionerFoundational")}>
+          <span>AWS</span>
+        </div>
+        <div className='step' onClick={() => handleQuizSelection("AwsDeveloperAssociate")}>
+          <span>DevOps</span>
+        </div>
+        <div className='step' onClick={() => handleQuizSelection("AzureAIFundamentalsBeginner")}>
+          <span>AI</span>
+        </div>
+      </div>
     </div>
   );
 }
